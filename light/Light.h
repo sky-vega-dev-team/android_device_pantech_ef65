cc6358bd19907379c1567321e0a63b4d0fcd7e3c
kongwoojin <kongwoojin03@gmail.com>
2020-04-02 09:59:09 +0000

    Revert "ef65: Sync lights HAL with onyx"
    This reverts commit 2467ec23d25a6ef1bf7ef2d55f3319d2ac4f4182.

--- light/Light.h
+++ light/Light.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,16 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #ifndef ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
 #define ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
 
 #include <android/hardware/light/2.0/ILight.h>
+#include <hardware/lights.h>
 #include <hidl/Status.h>
-
-#include <fstream>
+#include <map>
 #include <mutex>
-#include <unordered_map>
 
 namespace android {
 namespace hardware {
@@ -30,35 +28,23 @@ namespace light {
 namespace V2_0 {
 namespace implementation {
 
-struct Light : public ILight {
-    Light(std::ofstream&& lcd_backlight, std::pair<std::ofstream, std::ofstream>&& button_led,
-          std::ofstream&& red_led, std::ofstream&& green_led, std::ofstream&& blue_led);
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::light::V2_0::ILight;
+using ::android::hardware::light::V2_0::LightState;
+using ::android::hardware::light::V2_0::Status;
+using ::android::hardware::light::V2_0::Type;
 
-    // Methods from ::android::hardware::light::V2_0::ILight follow.
+class Light : public ILight {
+   public:
+    Light();
+
     Return<Status> setLight(Type type, const LightState& state) override;
     Return<void> getSupportedTypes(getSupportedTypes_cb _hidl_cb) override;
 
-  private:
-    void setAttentionLight(const LightState& state);
-    void setBatteryLight(const LightState& state);
-    void setButtonLight(const LightState& state);
-    void setLcdBacklight(const LightState& state);
-    void setNotificationLight(const LightState& state);
-    void setSpeakerBatteryLightLocked();
-    void setSpeakerLightLocked(const LightState& state);
-
-    std::ofstream mLcdBacklight;
-    std::pair<std::ofstream, std::ofstream> mButtonLed;
-    std::ofstream mRedLed;
-    std::ofstream mGreenLed;
-    std::ofstream mBlueLed;
-
-    LightState mAttentionState;
-    LightState mBatteryState;
-    LightState mNotificationState;
-
-    std::unordered_map<Type, std::function<void(const LightState&)>> mLights;
-    std::mutex mLock;
+   private:
+    std::mutex globalLock;
 };
 
 }  // namespace implementation
--- light/service.cpp
+++ light/service.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,83 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #define LOG_TAG "android.hardware.light@2.0-service.ef65"
-#include <android-base/logging.h>
+
 #include <hidl/HidlTransportSupport.h>
-#ifdef ARCH_ARM_32
-#include <hwbinder/ProcessState.h>
-#endif
-#include <utils/Errors.h>
+
 #include "Light.h"
 
-// libhwbinder:
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-// Generated HIDL files
 using android::hardware::light::V2_0::ILight;
 using android::hardware::light::V2_0::implementation::Light;
 
-const static std::string kLcdBacklightPath = "/sys/class/leds/lcd-backlight/brightness";
-const static std::string kButtonLedPath = "/sys/class/leds/kpdbl_menu/brightness";
-const static std::string kButtonLed1Path = "/sys/class/leds/kpdbl_back/brightness";
-const static std::string kRedLedPath = "/sys/class/leds/led:rgb_red/brightness";
-const static std::string kGreenLedPath = "/sys/class/leds/led:rgb_green/brightness";
-const static std::string kBlueLedPath = "/sys/class/leds/led:rgb_blue/brightness";
+using android::OK;
+using android::sp;
+using android::status_t;
 
 int main() {
-#ifdef ARCH_ARM_32
-    android::hardware::ProcessState::initWithMmapSize((size_t)(32768));
-#endif
-    std::ofstream lcdBacklight(kLcdBacklightPath);
-    if (!lcdBacklight) {
-        LOG(ERROR) << "Failed to open " << kLcdBacklightPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-    std::ofstream buttonLed(kButtonLedPath);
-    if (!buttonLed) {
-        LOG(ERROR) << "Failed to open " << kButtonLedPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-    std::ofstream buttonLed1(kButtonLed1Path);
-    if (!buttonLed1) {
-        LOG(ERROR) << "Failed to open " << kButtonLed1Path << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-    std::ofstream redLed(kRedLedPath);
-    if (!redLed) {
-        LOG(ERROR) << "Failed to open " << kRedLedPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-    std::ofstream greenLed(kGreenLedPath);
-    if (!greenLed) {
-        LOG(ERROR) << "Failed to open " << kGreenLedPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-    std::ofstream blueLed(kBlueLedPath);
-    if (!blueLed) {
-        LOG(ERROR) << "Failed to open " << kBlueLedPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-    android::sp<ILight> service = new Light(
-            std::move(lcdBacklight),
-            { std::move(buttonLed), std::move(buttonLed1) },
-            std::move(redLed), std::move(greenLed), std::move(blueLed));
+    android::sp<ILight> service = new Light();
+
     configureRpcThreadpool(1, true);
-    android::status_t status = service->registerAsService();
-    if (status != android::OK) {
-        LOG(ERROR) << "Cannot register Light HAL service";
+
+    status_t status = service->registerAsService();
+    if (status != OK) {
+        ALOGE("Cannot register Light HAL service.");
         return 1;
     }
-    LOG(INFO) << "Light HAL Ready.";
+
+    ALOGI("Light HAL service ready.");
+
     joinRpcThreadpool();
-    // Under normal cases, execution will not reach this line.
-    LOG(ERROR) << "Light HAL failed to join thread pool.";
+
+    ALOGI("Light HAL service failed to join thread pool.");
     return 1;
 }
